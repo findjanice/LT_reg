96a7a2335c3a90c3ded162a14cbd5b2bba9b8e14
Janice <findjanice@gmail.com>
2017-09-04 22:13:12 -0700

    added frontend

---
+++ public/login/loginCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+app.controller('loginCtrl', function($scope, $routeParams, $route, $location) {
+
+  this.state = $location.path();
+
+  this.go = function(path) {
+    $location.path(path);
+  }
+
+  $scope.login = function(data) {
+    registrationService.login(data).then(function(response) {
+      console.log('this is login response', response);
+      userId = response._id;
+      console.log('this is userId', userId);
+      if (response === "incorrect login") {
+        $scope.error = ""
+
+      } else {
+        $location.path("/register");
+      }
+
+    })
+  }
+
+  //end loginCtrl
+})
